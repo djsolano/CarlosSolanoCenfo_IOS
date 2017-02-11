fccc3f957c19bcf2dfc95fbdc0160bfacd432536
Carlos Solano <djsolano07@gmail.com>
2017-02-11 08:37:41 -0600

    Add files via upload

---
+++ Quiz2CSolano/Quiz2CSolano/ColorsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ColorsTableViewController.h
+//  Quiz2CSolano
+//
+//  Created by Estudiantes on 11/2/17.
+//  Copyright © 2017 Estudiantes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColorsTableViewController : UITableViewController
+
+@end
---
+++ Quiz2CSolano/Quiz2CSolano/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quiz2CSolano
+//
+//  Created by Estudiantes on 11/2/17.
+//  Copyright © 2017 Estudiantes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
