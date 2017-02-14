faec14c0849e31dd826fdb0a566eeed7e6ee7609
Carlos Solano <djsolano07@gmail.com>
2017-02-14 08:57:08 -0600

    Tarea 1

---
+++ Tarea1CarlosSolano/Tarea1CarlosSolano/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tarea1CarlosSolano
+//
+//  Created by Carlos Solano on 8/2/17.
+//  Copyright © 2017 Carlos Solano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tarea1CarlosSolano/Tarea1CarlosSolano/NumbersTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NumbersTableViewController.h
+//  Tarea1CarlosSolano
+//
+//  Created by Carlos Solano on 8/2/17.
+//  Copyright © 2017 Carlos Solano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NumbersTableViewController : UITableViewController
+
+@end
