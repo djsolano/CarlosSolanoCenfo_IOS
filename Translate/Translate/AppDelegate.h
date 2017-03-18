a249bc2377cab71ffba69dd9e0c4373752bc1e7e
Carlos Solano <djsolano07@gmail.com>
2017-03-18 12:02:35 -0600

    Translate con stack views

---
+++ Translate/Translate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Translate
+//
+//  Created by Estudiantes on 18/3/17.
+//  Copyright © 2017 Estudiantes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
